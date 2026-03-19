9dfc1d62f635eb3e1a1239db288956262865ca94
yoan77 <morenodasilvajoao709@gmail.com>
2026-03-19 17:22:54 -0300

    feat: make bind between CrudContract and EventRepository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Contracts\CrudContract;
+use App\Repositories\EventRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CrudContract::class, EventRepository::class);
     }
 
     /**
