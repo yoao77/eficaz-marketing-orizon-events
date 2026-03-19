98707a9986e8559f9b638cc6b4bccb23993b7d39
yoan77 <morenodasilvajoao709@gmail.com>
2026-03-19 16:28:03 -0300

    feat: add EventRepository with implementation in CrudContract

---
+++ app/Repositories/EventRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\CrudContract;
+use App\Models\Event;
+
+class EventRepository implements CrudContract
+{
+    public function __construct(private Event $event) {}
+
+    public function index()
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function show(int $id)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function store(array $data)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function update(int $id, array $data)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function destroy(int $id)
+    {
+        throw new \Exception('Not implemented');
+    }
+}
