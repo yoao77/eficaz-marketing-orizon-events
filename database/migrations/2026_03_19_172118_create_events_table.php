bad510de36a3b5c8a6d4863e5a7bd82492dac183
yoan77 <morenodasilvajoao709@gmail.com>
2026-03-19 14:39:18 -0300

    feat: add events migration

---
+++ database/migrations/2026_03_19_172118_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->dateTime('event_datetime');
+            $table->integer('people_capacity')->nullable();
+            $table->enum('status', ['active', 'canceled'])->default('active');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
