c08db95f79e490725f1d5df26b1b64d3a80f2a99
yoan77 <morenodasilvajoao709@gmail.com>
2026-03-19 17:33:49 -0300

    feat: call all repository methods in EventService

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -4,8 +4,32 @@ namespace App\Services;
 
 use App\Contracts\CrudContract;
 
-class EventService {
-
+class EventService
+{
     public function __construct(private CrudContract $repository) {}
 
+    public function index()
+    {
+        return $this->repository->index();
+    }
+
+    public function show(int $id)
+    {
+        return $this->repository->show($id);
+    }
+
+    public function store(array $data)
+    {
+        return $this->repository->store($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    public function destroy(int $id)
+    {
+        return $this->repository->destroy($id);
+    }
 }
