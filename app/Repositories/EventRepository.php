fab1e2bdfbdcaae567c1f1f5e6ce855aff78851b
yoan77 <morenodasilvajoao709@gmail.com>
2026-03-19 17:11:15 -0300

    feat: implement update event

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -36,7 +36,17 @@ class EventRepository implements CrudContract
 
     public function update(int $id, array $data)
     {
-        throw new \Exception('Not implemented');
+        $event = $this->event->findOrFail($id);
+
+        $event->update([
+            'user_id' => $data['user_id'] ?? $event->user_id,
+            'title' => $data['title'] ?? $event->title,
+            'description' => $data['description'] ?? $event->description,
+            'location' => $data['location'] ?? $event->location,
+            'event_datetime' => $data['event_datetime'] ?? $event->event_datetime,
+            'people_capacity' => $data['people_capacity'] ?? $event->people_capacity,
+            'status' => $data['status'] ?? $event->status,
+        ]);
     }
 
     public function destroy(int $id)
