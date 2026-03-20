4f434138853d7f14f36c36f22bc45c53d37e8f45
yoan77 <morenodasilvajoao709@gmail.com>
2026-03-20 10:46:13 -0300

    feat: add StoreEventRequest and rules

---
+++ app/Http/Requests/StoreEventRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreEventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'location' => 'required|string|max:255',
+            'event_datetime' => 'required|date|after:now',
+            'people_capacity' => 'nullable|integer|min:1',
+            'status' => 'nullable|in:active,canceled',
+        ];
+    }
+}
