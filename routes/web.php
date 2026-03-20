2856d45b579b66718693ba1a9dcf1d82d111ee70
yoan77 <morenodasilvajoao709@gmail.com>
2026-03-20 10:28:39 -0300

    feat: add events resource in web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,27 +16,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('events', EventController::class)->names([
+        'index'   => 'events.index',
+        'store'   => 'events.store',
+        'show'    => 'events.show',
+        'update'  => 'events.update',
+        'destroy' => 'events.destroy',
+    ]);
 });
-
-Route::get('/my-events', function () {
-    $events = [
-        (object)[
-            'title' => 'Conferência Biohit 2026',
-            'description' => 'Discussão sobre relatórios automatizados e integração com Excel.',
-            'location' => 'São Paulo, SP',
-            'date_time' => '2026-05-20 09:00:00',
-            'status' => 'active'
-        ],
-        (object)[
-            'title' => 'Data Sloth Workshop',
-            'description' => 'Treinamento prático de ingestão de dados com Kafka.',
-            'location' => 'Remoto',
-            'date_time' => '2026-06-15 14:30:00',
-            'status' => 'pending'
-        ],
-    ];
-
-    return view('events.index', compact('events'));
-})->middleware(['auth']);
 
 require __DIR__ . '/auth.php';
