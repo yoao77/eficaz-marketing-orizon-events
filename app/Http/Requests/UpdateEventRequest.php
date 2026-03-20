6ac89edfa09edb33de1ea26f294678164d14beed
yoan77 <morenodasilvajoao709@gmail.com>
2026-03-20 15:03:18 -0300

    feat: add update event request and put rules

---
+++ app/Http/Requests/UpdateEventRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'location' => 'required|string|max:255',
+            'event_datetime' => 'required|date|after_or_equal:now',
+            'people_capacity' => 'nullable|integer|min:1',
+            'status' => 'required|in:active,canceled',
+        ];
+    }
+}
