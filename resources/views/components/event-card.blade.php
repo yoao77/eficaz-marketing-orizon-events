54f8ec0995b3529b9e97385e6e33485560b37fc2
jpMoreno12 <morenodasilvajoao709@gmail.com>
2026-03-20 02:40:50 -0300

    feat: add edit modal

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -38,7 +38,11 @@
             </svg>
         </button>
 
-        <button title="Edit" class="p-2 text-yellow-600 hover:bg-yellow-50 rounded-md transition">
+        <button title="Edit"
+            type="button"
+            {{-- Aqui está a mágica: passamos o objeto $event para a função global --}}
+            onclick='setupEditModal(@json($event))'
+            class="p-2 text-yellow-600 hover:bg-yellow-50 rounded-md transition">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
             </svg>
