b8e4e2d8543c90c4e6a8cc099a7478663164c055
jpMoreno12 <morenodasilvajoao709@gmail.com>
2026-03-21 03:44:21 -0300

    feat: add EventUser seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserSeeder::class);
         $this->call(EventSeeder::class);
+        $this->call(EventUserSeeder::class);
     }
 }
---
+++ database/seeders/EventUserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EventUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $events = Event::all();
+        $users = User::all();
+
+        if ($events->isEmpty() || $users->isEmpty()) {
+            $this->command->info('Run UserSeeder and EventSeeder first!');
+            return;
+        }
+
+        $events[0]->participants()->attach([
+            $users[0]->id,
+            $users[1]->id,
+            $users[2]->id,
+        ]);
+
+        $events[1]->participants()->attach([
+            $users[0]->id,
+            $users[1]->id,
+        ]);
+
+        $events[2]->participants()->attach([
+            $users[2]->id,
+        ]);
+    }
+}
