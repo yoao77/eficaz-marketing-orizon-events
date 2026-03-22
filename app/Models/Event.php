9bde3226e2f31ba4dea079ee20b31d67c6e20e46
jpMoreno12 <morenodasilvajoao709@gmail.com>
2026-03-22 11:32:38 -0300

    feat: add isUserSubscribed method in EventModel

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -54,4 +54,11 @@ class Event extends Model
         return !is_null($this->people_capacity) &&
             $this->participants_count >= $this->people_capacity;
     }
+
+    public function isUserSubscribed(?int $userId): bool
+    {
+        if (!$userId) return false;
+
+        return $this->participants->contains('id', $userId);
+    }
 }
